e1e2830ff019f23e22a32665c17781347e8221ec
Kaz <malkazwini@gmail.com>
2017-02-28 10:08:08 -0600

    Source: Digital Ocean

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+sudo apt-get update
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+sudo apt-add-repository 'deb https://apt.dockerproject.org/repo ubuntu-xenial main'
+sudo apt-get update
+apt-cache policy docker-engine
+sleep 5
+sudo apt-get install -y docker-engine
+sudo systemctl status docker
+echo "####### Removing sudo requirement to run Docker ##################" 
+sudo usermod -aG docker $(whoami)
+
